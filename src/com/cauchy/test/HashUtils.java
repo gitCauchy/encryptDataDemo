7f07add90ac92f04cc1d8aa960b2415e0c5573d4
Cauchy <Cauchy@windows10.microdone.cn>
2019-09-12 09:54:09 +0800

    更新散列算法

--- src/com/cauchy/test/HashUtils.java
+++ src/com/cauchy/test/HashUtils.java
@@ -4,7 +4,9 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class HashUtils {
-	private static final char[] HEX_DIGITS = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+	private static final char[] HEX_DIGITS = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D',
+			'E', 'F' };
+
 	/**
 	 * @description MD5计算散列
 	 */
@@ -19,14 +21,16 @@ public class HashUtils {
 		messageDigest.update(data.getBytes());
 		return byteArray2HexString(messageDigest.digest());
 	}
+
 	private static String byteArray2HexString(byte[] bytes) {
 		StringBuilder stringBuilder = new StringBuilder();
-		for(byte b : bytes) {
+		for (byte b : bytes) {
 			stringBuilder.append(HEX_DIGITS[(b & 0xf0) >> 4]).append(HEX_DIGITS[(b & 0x0f)]);
 		}
 		return stringBuilder.toString();
 	}
-    public static void main(String[] args) {
-        System.out.println(getMD5String("123456"));
-    }
+
+	public static void main(String[] args) {
+		System.out.println(getMD5String("123456"));
+	}
 }
